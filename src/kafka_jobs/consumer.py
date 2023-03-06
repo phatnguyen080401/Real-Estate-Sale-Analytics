82d8a56db98c9a1cc045193a3b55012002402301
phatnguyen080401 <phat.nguyen@c2c-techhub.io>
2023-03-06 12:07:32 +0700

    Change Cassandra to Snowflake database

--- src/kafka_jobs/consumer.py
+++ src/kafka_jobs/consumer.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append(".")
 
 import json
-import uuid
+from datetime import datetime
 
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
@@ -14,8 +14,15 @@ from logger.logger import Logger
 KAFKA_ENDPOINT = "{0}:{1}".format(config['KAFKA']['KAFKA_ENDPOINT'], config['KAFKA']['KAFKA_ENDPOINT_PORT'])
 KAFKA_TOPIC    = config['KAFKA']['KAFKA_TOPIC']
 
-CLUSTER_ENDPOINT = "{0}:{1}".format(config['CASSANDRA']['CLUSTER_HOST'], config['CASSANDRA']['CLUSTER_PORT'])
-CLUSTER_KEYSPACE = config['CASSANDRA']['CLUSTER_KEYSPACE']
+SNOWFLAKE_OPTIONS = {
+    "sfURL" : config['SNOWFLAKE']['URL'],
+    "sfAccount": config['SNOWFLAKE']['ACCOUNT'],
+    "sfUser" : config['SNOWFLAKE']['USER'],
+    "sfPassword" : config['SNOWFLAKE']['PASSWORD'],
+    "sfDatabase" : config['SNOWFLAKE']['DATABASE'],
+    "sfSchema" : config['SNOWFLAKE']['SCHEMA'],
+    "sfWarehouse" : config['SNOWFLAKE']['WAREHOUSE']
+}
 
 logger = Logger('Kafka-Consumer')
 
@@ -24,9 +31,12 @@ class Consumer:
     self._spark = SparkSession \
             .builder \
             .master("local[*]") \
-            .appName("Streaming") \
-            .config("spark.cassandra.connection.host", CLUSTER_ENDPOINT) \
-            .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0,com.datastax.spark:spark-cassandra-connector_2.12:3.1.0") \
+            .appName("Consumer") \
+            .config("spark.jars.packages", 
+                    "org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0," +
+                    "net.snowflake:snowflake-jdbc:3.13.14," + 
+                    "net.snowflake:spark-snowflake_2.12:2.11.0-spark_3.2"
+                  ) \
             .getOrCreate()
     
     self._spark.sparkContext.setLogLevel("ERROR")
@@ -50,17 +60,17 @@ class Consumer:
 
     return df
 
-  def save_to_cassandra_lake(self, batch_df, batch_id):
+  def save_to_data_lake(self, batch_df, batch_id):
     schema = StructType([
-                  StructField("vendor_id", LongType(), True),
+                  StructField("VendorID", IntegerType(), True),
                   StructField("tpep_pickup_datetime", StringType(), True),
                   StructField("tpep_dropoff_datetime", StringType(), True),
                   StructField("passenger_count", DoubleType(), True),
                   StructField("trip_distance", DoubleType(), True),
-                  StructField("ratecode_id", DoubleType(), True),
+                  StructField("RatecodeID", DoubleType(), True),
                   StructField("store_and_fwd_flag", StringType(), True),
-                  StructField("pu_location_id", LongType(), True),
-                  StructField("do_location_id", LongType(), True),
+                  StructField("PULocationID", IntegerType(), True),
+                  StructField("DOLocationID", IntegerType(), True),
                   StructField("payment_type", LongType(), True),
                   StructField("fare_amount", DoubleType(), True),
                   StructField("extra", DoubleType(), True),
@@ -76,15 +86,23 @@ class Consumer:
     try:
       records = batch_df.count()
 
-      uuid_generator = udf(lambda: str(uuid.uuid4()), StringType())
-
-      parse_df = batch_df.rdd.map(lambda x: Consumer.parse(json.loads(x.value))).toDF(schema)
-      parse_df = parse_df.withColumn("id", uuid_generator())
+      parse_df = batch_df.rdd \
+                          .filter(lambda x: len(json.loads(x.value)) == 19) \
+                          .map(lambda x: json.loads(x.value)) \
+                          .toDF(schema)
+      parse_df = parse_df \
+                    .withColumn("created_at", lit(datetime.now())) \
+                    .withColumnRenamed("VendorID","vendor_id") \
+                    .withColumnRenamed("RatecodeID","ratecode_id") \
+                    .withColumnRenamed("PULocationID","pu_location_id") \
+                    .withColumnRenamed("DOLocationID","do_location_id")
 
       parse_df \
           .write \
-          .format("org.apache.spark.sql.cassandra") \
-          .options(table="data_lake", keyspace=CLUSTER_KEYSPACE) \
+          .format("snowflake") \
+          .options(**SNOWFLAKE_OPTIONS) \
+          .option("dbtable", "data_lake") \
+          .options(header=True) \
           .mode("append") \
           .save()
 
@@ -92,87 +110,14 @@ class Consumer:
     except Exception as e:
       logger.error(e)
 
-  @staticmethod
-  def parse(raw_data):   
-    vendor_id = raw_data["VendorID"]
-    tpep_pickup_datetime = raw_data["tpep_pickup_datetime"]
-    tpep_dropoff_datetime = raw_data["tpep_dropoff_datetime"]
-    trip_distance = raw_data["trip_distance"]
-    pu_location_id = raw_data["PULocationID"]
-    do_location_id = raw_data["DOLocationID"]
-    payment_type = raw_data["payment_type"]
-    fare_amount = raw_data["fare_amount"]
-    extra = raw_data["extra"]
-    mta_tax = raw_data["mta_tax"]
-    tip_amount = raw_data["tip_amount"]
-    tolls_amount = raw_data["tolls_amount"]
-    improvement_surcharge = raw_data["improvement_surcharge"]
-    total_amount = raw_data["total_amount"]
-
-    if "ratecode_id" in raw_data:
-      ratecode_id = raw_data["ratecode_id"]
-    else:
-      ratecode_id = None
-
-    if "store_and_fwd_flag" in raw_data:
-      store_and_fwd_flag = raw_data["store_and_fwd_flag"]
-    else:
-      store_and_fwd_flag = None
-
-    if "payment_type" in raw_data:
-      payment_type = raw_data["payment_type"]
-    else:
-      payment_type = None
-
-    if "passenger_count" in raw_data:
-      passenger_count = raw_data["passenger_count"]
-    else:
-      passenger_count = None
-
-    if "congestion_surcharge" in raw_data:
-      congestion_surcharge = raw_data["congestion_surcharge"]
-    else:
-      congestion_surcharge = None
-
-    if "airport_fee" in raw_data:
-      airport_fee = raw_data["airport_fee"]
-    else:
-      airport_fee = None
-
-    data = {
-              "vendor_id": vendor_id,
-              "tpep_pickup_datetime": tpep_pickup_datetime,
-              "tpep_dropoff_datetime": tpep_dropoff_datetime, 
-              "passenger_count": passenger_count, 
-              "trip_distance": trip_distance,
-              "ratecode_id": ratecode_id,
-              "store_and_fwd_flag": store_and_fwd_flag,
-              "pu_location_id": pu_location_id,
-              "do_location_id": do_location_id,
-              "payment_type": payment_type, 
-              "fare_amount": fare_amount, 
-              "extra": extra,
-              "mta_tax": mta_tax,
-              "tip_amount": tip_amount,
-              "tolls_amount": tolls_amount,
-              "improvement_surcharge": improvement_surcharge,
-              "total_amount": total_amount,
-              "congestion_surcharge": congestion_surcharge,
-              "airport_fee": airport_fee
-            }
-
-    return data
-
   def run(self):
     try:
       df = self.comsume_from_kafka()
 
-      # tweets = df.select(('value')).alias("data").select("data.*")
-
       stream = df \
             .writeStream \
             .trigger(processingTime='5 seconds') \
-            .foreachBatch(self.save_to_cassandra_lake) \
+            .foreachBatch(self.save_to_data_lake) \
             .outputMode("append") \
             .start()
 
