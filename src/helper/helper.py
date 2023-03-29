32655f143ca066a07f338a3c94516c13ecd2e8d1
phatnguyen080401 <phat.nguyen@c2c-techhub.io>
2023-03-29 15:49:40 +0700

    Remove functions

--- src/helper/helper.py
+++ src/helper/helper.py
@@ -1,9 +1,5 @@
 import os
 import re
-import shutil
-from urllib.error import HTTPError
-from urllib import request
-import dask.dataframe as dd
 
 class Helper:
 
@@ -37,21 +33,3 @@ class Helper:
 
   def get_str_between(self, str, str1, str2):
     return re.findall(r'{}(.*?){}'.format(str1, str2), str)
-
-  def download_from_url(self, file_name, url):
-    try:
-      request.urlretrieve(url, f"./data_source/{file_name}.parquet")
-    except HTTPError:
-      print(f"File {file_name}.parquet not exist") 
-
-  def split_file(self, file_name, schema, partition=100):
-    name_function = lambda x: f"{file_name}-{x}.parquet"
-    ddf = dd.read_parquet(f"./data_source/{file_name}.parquet")
-    sorted_ddf = ddf.sort_values("tpep_pickup_datetime")
-    sorted_ddf.repartition(partition).to_parquet("tmp/", name_function=name_function, schema=schema)
-
-  def move_file(self, source, destination, file_name):
-    shutil.move(f"./{source}/{file_name}.parquet", f"./{destination}/{file_name}.parquet")
-
-  def main():
-    pass
