0f26900431d795ed7fd81c81d5a46da3824d8bd8
phatnguyen080401 <phat.nguyen@c2c-techhub.io>
2023-04-06 16:50:49 +0700

    Change AIRFLOW_UID to root user

--- scripts/get-uid-gid-airflow.sh
+++ scripts/get-uid-gid-airflow.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
 cd ./deploy
-echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
+# echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
+echo -e "AIRFLOW_UID=0\nAIRFLOW_GID=0" > .env
