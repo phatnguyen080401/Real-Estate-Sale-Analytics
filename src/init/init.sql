3ba4d94ff8d09ed263eccbf6d844cf27a3e83017
phatnguyen080401 <phat.nguyen@c2c-techhub.io>
2023-05-03 18:00:41 +0700

    Create database

--- src/init/init.sql
+++ src/init/init.sql
@@ -1,111 +1,91 @@
 -- Create virtual warehouse
 USE ROLE ACCOUNTADMIN;
 
-CREATE OR REPLACE WAREHOUSE nyc_wh
-  WITH WAREHOUSE_SIZE = 'XSMALL'
+CREATE OR REPLACE WAREHOUSE real_estate_sales_wh
+  WITH WAREHOUSE_SIZE = 'MEDIUM'
   AUTO_SUSPEND = 120
   AUTO_RESUME = true
   INITIALLY_SUSPENDED = TRUE;
 
 -- Create database
-CREATE OR REPLACE DATABASE nyc_db;
+CREATE OR REPLACE DATABASE sale_db;
 
 -- Create schema
-CREATE OR REPLACE SCHEMA nyc_lake;
-CREATE OR REPLACE SCHEMA yellow_taxi_batch;
-CREATE OR REPLACE SCHEMA yellow_taxi_speed;
+CREATE OR REPLACE SCHEMA sale_lake;
+CREATE OR REPLACE SCHEMA sale_batch;
+CREATE OR REPLACE SCHEMA sale_speed;
 
 -- Create tables
+
+-- schema: sale_lake
 -- table: data_lake
-CREATE OR REPLACE TABLE nyc_db.nyc_lake.data_lake (
-	vendor_id SMALLINT,
-	tpep_pickup_datetime TEXT,
-	tpep_dropoff_datetime TEXT,
-	passenger_count FLOAT,
-	trip_distance FLOAT,
-	ratecode_id FLOAT,
-	store_and_fwd_flag TEXT,
-	pu_location_id SMALLINT,
-	do_location_id SMALLINT,
-	payment_type SMALLINT,
-	fare_amount FLOAT,
-	extra FLOAT,
-	mta_tax FLOAT,
-	tip_amount FLOAT,
-	tolls_amount FLOAT,
-	improvement_surcharge FLOAT,
-	total_amount FLOAT,
-	congestion_surcharge FLOAT,
-	airport_fee FLOAT,
+CREATE OR REPLACE TABLE sale_db.sale_lake.data_lake (
+	serial_number BIGINT,
+	list_year SMALLINT,
+	date_recorded DATE,
+	town TEXT,
+	address TEXT,
+	assessed_value FLOAT,
+	sale_amount FLOAT,
+	sales_ratio FLOAT,
+	property_type TEXT,
+	residential_type TEXT,
+	non_use_code TEXT,
+	assessor_remarks TEXT,
+	opm_remarks TEXT,
+	location TEXT,
 	created_at TIMESTAMP_NTZ(9)
 );
 
--- schema: yellow_taxi_batch
--- table: total_trip_distance
-CREATE OR REPLACE TABLE nyc_db.yellow_taxi_batch.total_trip_distance (
-    total_trip_distance FLOAT,
-    total_rides BIGINT,
+-- schema: sale_batch
+-- table: total_sale_amount_ratio
+CREATE OR REPLACE TABLE sale_db.sale_batch.total_sale_amount_ratio (
+    total_sale_amount FLOAT,
+    total_sales_ratio FLOAT,
+    total_customer BIGINT,
     started_at TIMESTAMP_NTZ(9),
     ended_at TIMESTAMP_NTZ(9)
 );
 
--- schema: yellow_taxi_batch
--- table: total_amount
-CREATE OR REPLACE TABLE nyc_db.yellow_taxi_batch.total_amount (
-    total_amount FLOAT,
-    total_rides BIGINT,
+-- schema: sale_batch
+-- table: total_customer_by_property_type
+CREATE OR REPLACE TABLE sale_db.sale_batch.total_customer_by_property_type (
+    property_type TEXT,
+    total_customer BIGINT,
     started_at TIMESTAMP_NTZ(9),
     ended_at TIMESTAMP_NTZ(9)
 );
 
--- schema: yellow_taxi_batch
--- table: total_passenger
-CREATE OR REPLACE TABLE nyc_db.yellow_taxi_batch.total_passenger (
-    total_passenger FLOAT,
-    total_rides BIGINT,
+-- schema: sale_batch
+-- table: total_customer_by_town
+CREATE OR REPLACE TABLE sale_db.sale_batch.total_customer_by_town (
+    town TEXT,
+    total_customer BIGINT,
     started_at TIMESTAMP_NTZ(9),
     ended_at TIMESTAMP_NTZ(9)
 );
 
--- schema: yellow_taxi_speed
--- table: pickup_dropoff
-CREATE OR REPLACE TABLE nyc_db.yellow_taxi_speed.pickup_dropoff (
-    vendor_id FLOAT,
-    tpep_pickup_datetime TIMESTAMP_NTZ(9),
-    tpep_dropoff_datetime TIMESTAMP_NTZ(9),
-    pu_location_id FLOAT,
-    do_location_id FLOAT,
+-- schema: sale_speed
+-- table: total_sale_amount_ratio
+CREATE OR REPLACE TABLE sale_db.sale_speed.total_sale_amount_ratio (
+    total_sale_amount FLOAT,
+    total_sales_ratio FLOAT,
+    total_customer BIGINT,
     created_at TIMESTAMP_NTZ(9)
 ) CLUSTER BY (created_at);
 
--- schema: yellow_taxi_speed
--- table: user_per_payment
-CREATE OR REPLACE TABLE nyc_db.yellow_taxi_speed.user_per_payment (
-    vendor_id FLOAT,
-    payment_type FLOAT,
+-- schema: sale_speed
+-- table: total_per_property_type
+CREATE OR REPLACE TABLE sale_db.sale_speed.total_per_property_type (
+    property_type TEXT,
+    total_customer BIGINT,
     created_at TIMESTAMP_NTZ(9)
 ) CLUSTER BY (created_at);
 
--- schema: yellow_taxi_speed
--- table: total_amount
-CREATE OR REPLACE TABLE nyc_db.yellow_taxi_speed.total_amount (
-    total_amount FLOAT,
-    total_rides BIGINT,
-    created_at TIMESTAMP_NTZ(9)
-) CLUSTER BY (created_at);
-
--- schema: yellow_taxi_speed
--- table: total_trip_distance
-CREATE OR REPLACE TABLE nyc_db.yellow_taxi_speed.total_trip_distance (
-    total_trip_distance FLOAT,
-    total_rides BIGINT,
-    created_at TIMESTAMP_NTZ(9)
-) CLUSTER BY (created_at);
-
--- schema: yellow_taxi_speed
--- table: total_passenger
-CREATE OR REPLACE TABLE nyc_db.yellow_taxi_speed.total_passenger (
-    total_passenger FLOAT,
-    total_rides BIGINT,
+-- schema: sale_speed
+-- table: total_customer_by_town
+CREATE OR REPLACE TABLE sale_db.sale_speed.total_customer_by_town (
+    town TEXT,
+    total_customer BIGINT,
     created_at TIMESTAMP_NTZ(9)
 ) CLUSTER BY (created_at);
