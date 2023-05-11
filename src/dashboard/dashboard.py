f83dc4b46832ce0151200baef7f1fe8f0fcc9546
phatnguyen080401 <phat.nguyen@c2c-techhub.io>
2023-05-11 14:21:25 +0700

    Real-time dashboard

--- src/dashboard/dashboard.py
+++ src/dashboard/dashboard.py
@@ -1,167 +1,273 @@
 import time  
 import numpy as np  
 import pandas as pd  
-import seaborn as sns
-import matplotlib.pyplot as plt
 import plotly.express as px 
 import streamlit as st  
 from datetime import datetime
-from cassandra.cluster import Cluster
+
+import snowflake.connector
+from snowflake.connector import errors, errorcode
 
 st.set_page_config(
-  page_title="Real-Time NYC Taxi Dashboard",
+  page_title="Real Estate Sales Dashboard",
   page_icon="✅",
   layout="wide",
 )
 
-session = Cluster().connect()
-session.set_keyspace("nyc_taxi")
+config = {
+    "user": "dbadmin",
+    "password": "123456",
+    "account": "tl16901",
+    "region": "ap-southeast-1",
+    "database": "sale_db",
+    "warehouse": "real_estate_sales_wh",
+    "role":"DBADMIN"
+}
 
-def get_average_trip_distance():
-  batch_query = '''
-                  SELECT total_trip_distance, total_rides, ended_at, started_at
-                  FROM total_trip_distance_batch 
-                  LIMIT 1 ;
-                '''
-  batch_data = session.execute(batch_query).all()
-  total_trip_distance_batch, total_rides_batch, ended_at, started_at  = batch_data[0]
+def query_total_sale_amount(conn_config):
+  try:
+    conn = snowflake.connector.connect(**conn_config)
+    cursor = conn.cursor()
 
-  speed_query = f'''
-                  SELECT SUM(trip_distance) as total_trip_distance, 
-                         COUNT(*) as total_rides 
-                  FROM total_trip_distance_speed 
-                  WHERE created_at >= '{started_at}' 
-                    AND created_at <= '{datetime.now()}' 
-                  ALLOW FILTERING ;
-                '''
-  speed_data = session.execute(speed_query).all()
-  total_trip_distance_speed, total_rides_speed = speed_data[0]
+    query_batch = '''
+        SELECT 
+          total_sale_amount, 
+          started_at
+        FROM sale_batch.total_sale_amount_ratio
+        ORDER BY ended_at DESC
+        LIMIT 1
+    '''
+    total_sale_amount_1, started_at = cursor.execute(query_batch).fetchone()
 
-  average_distance = (total_trip_distance_batch + total_trip_distance_speed) / (total_rides_batch + total_rides_speed)
+    query_speed = f'''
+        SELECT 
+          SUM(total_sale_amount)
+        FROM sale_speed.total_sale_amount_ratio
+        WHERE 
+          created_at >= '{started_at}' 
+          AND created_at <= '{datetime.now()}' 
+    '''
+    total_sale_amount_2 = cursor.execute(query_speed).fetchone()[0]
 
-  return average_distance
+    total_sale_amount = total_sale_amount_1 + total_sale_amount_2
 
-def get_average_total_amount():
-  batch_query = '''
-                  SELECT total_amount, total_rides, ended_at, started_at
-                  FROM total_amount_batch 
-                  LIMIT 1 ;
-                '''
-  batch_data = session.execute(batch_query).all()
-  total_amount_batch, total_rides_batch, ended_at, started_at  = batch_data[0]
+    return total_sale_amount
+  except errors.Error as err:
+    if err.errno == errorcode.ER_FAILED_TO_CONNECT_TO_DB:
+      print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_NO_ACCOUNT_NAME:
+      print("Missing account name")
+    elif err.errno == errorcode.ER_NO_USER:
+      print("Missing username")
+    elif err.errno == errorcode.ER_NO_PASSWORD:
+      print("Missing password")
+    else:
+      print(err)
+  else:
+    conn.close()
 
-  speed_query = f'''
-                  SELECT SUM(total_amount) as total_amount, 
-                         COUNT(*) as total_rides 
-                  FROM total_amount_speed 
-                  WHERE created_at >= '{started_at}' 
-                    AND created_at <= '{datetime.now()}' 
-                  ALLOW FILTERING ;
-                '''
-  speed_data = session.execute(speed_query).all()
-  total_amount_speed, total_rides_speed = speed_data[0]
+def query_total_customer(conn_config):
+  try:
+    conn = snowflake.connector.connect(**conn_config)
+    cursor = conn.cursor()
 
-  average_total_amount = (total_amount_batch + total_amount_speed) / (total_rides_batch + total_rides_speed)
+    query_batch = '''
+        SELECT 
+          total_customer, 
+          started_at
+        FROM sale_batch.total_sale_amount_ratio
+        ORDER BY ended_at DESC
+        LIMIT 1
+    '''
+    total_customer_1, started_at = cursor.execute(query_batch).fetchone()
 
-  return average_total_amount
+    query_speed = f'''
+        SELECT 
+          SUM(total_customer)
+        FROM sale_speed.total_sale_amount_ratio
+        WHERE 
+          created_at >= '{started_at}' 
+          AND created_at <= '{datetime.now()}' 
+    '''
+    total_customer_2 = cursor.execute(query_speed).fetchone()[0]
 
-def get_total_passenger():
-  batch_query = '''
-                  SELECT total_passenger, total_rides, ended_at, started_at 
-                  FROM total_passenger_batch 
-                  LIMIT 1 ;
-                '''
-  batch_data = session.execute(batch_query).all()
-  total_passenger_batch, total_rides_batch, ended_at, started_at  = batch_data[0]
+    total_customer = total_customer_1 + total_customer_2
 
-  speed_query = f'''
-                  SELECT SUM(passenger_count) as total_passenger, 
-                         COUNT(*) as total_rides 
-                  FROM total_passenger_speed 
-                  WHERE created_at >= '{started_at}' 
-                    AND created_at <= '{datetime.now()}' 
-                  ALLOW FILTERING ;
-                '''
-  speed_data = session.execute(speed_query).all()
-  total_passenger_speed, total_rides_speed = speed_data[0]
+    return total_customer
+  except errors.Error as err:
+    if err.errno == errorcode.ER_FAILED_TO_CONNECT_TO_DB:
+      print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_NO_ACCOUNT_NAME:
+      print("Missing account name")
+    elif err.errno == errorcode.ER_NO_USER:
+      print("Missing username")
+    elif err.errno == errorcode.ER_NO_PASSWORD:
+      print("Missing password")
+    else:
+      print(err)
+  else:
+    conn.close()
 
-  average_passenger = total_passenger_batch + total_passenger_speed
+def query_average_sale_ratio(conn_config):
+  try:
+    conn = snowflake.connector.connect(**conn_config)
+    cursor = conn.cursor()
 
-  return average_passenger
+    query_batch = '''
+        SELECT 
+          total_sales_ratio,
+          total_customer,
+          started_at
+        FROM sale_batch.total_sale_amount_ratio
+        ORDER BY ended_at DESC
+        LIMIT 1
+    '''
+    total_sales_ratio_1, total_customer_1, started_at = cursor.execute(query_batch).fetchone()
 
-def get_top10_pickup_districts():
-  speed_query = f'''
-                  SELECT vendor_id, 
-                         pu_location_id 
-                  FROM pickup_dropoff_speed ;
-                '''
-  speed_data = session.execute(speed_query)
-  pickup_df = pd.DataFrame(speed_data)
+    query_speed = f'''
+        SELECT 
+          SUM(total_sales_ratio),
+          SUM(total_customer)
+        FROM sale_speed.total_sale_amount_ratio
+        WHERE 
+          created_at >= '{started_at}' 
+          AND created_at <= '{datetime.now()}' 
+    '''
+    total_sales_ratio_2, total_customer_2 = cursor.execute(query_speed).fetchone()
 
-  top10_pickup_districts_df = pickup_df \
-                              .groupby('pu_location_id', as_index=False) \
-                              .count() \
-                              .rename({'vendor_id' : 'count'}, axis=1) \
-                              .sort_values('count', ascending=False) \
-                              .head(10)
+    avg_sale_ratio = (total_sales_ratio_1 + total_sales_ratio_2) / (total_customer_1 + total_customer_2)
 
-  taxi_zone_lookup_df = pd.read_csv("./data_source/taxi_zone_lookup.csv")
-  top10_pickup_districts_df = top10_pickup_districts_df.merge(
-          taxi_zone_lookup_df,
-          left_on='pu_location_id',
-          right_on='LocationID',
-          how='left'
-  )
+    return avg_sale_ratio
+  except errors.Error as err:
+    if err.errno == errorcode.ER_FAILED_TO_CONNECT_TO_DB:
+      print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_NO_ACCOUNT_NAME:
+      print("Missing account name")
+    elif err.errno == errorcode.ER_NO_USER:
+      print("Missing username")
+    elif err.errno == errorcode.ER_NO_PASSWORD:
+      print("Missing password")
+    else:
+      print(err)
+  else:
+    conn.close()
 
-  return top10_pickup_districts_df
+def query_total_customer_by_property_type(conn_config):
+  try:
+    conn = snowflake.connector.connect(**conn_config)
+    cursor = conn.cursor()
 
-def get_top10_dropoff_districts():
-  speed_query = f'''
-                  SELECT vendor_id, 
-                         do_location_id 
-                  FROM pickup_dropoff_speed ;
-                '''
-  speed_data = session.execute(speed_query)
-  dropoff_df = pd.DataFrame(speed_data)
+    query_batch = '''
+        SELECT 
+          property_type,
+          SUM(total_customer),
+          started_at
+        FROM sale_batch.total_customer_by_property_type
+        GROUP BY 
+          property_type, 
+          started_at;
+    '''
+    df_1 = cursor.execute(query_batch).fetch_pandas_all()
 
-  top10_dropoff_districts_df = dropoff_df \
-                              .groupby('do_location_id', as_index=False) \
-                              .count() \
-                              .rename({'vendor_id' : 'count'}, axis=1) \
-                              .sort_values('count', ascending=False) \
-                              .head(10)
+    started_at = df_1.iloc[0, 2]
 
-  taxi_zone_lookup_df = pd.read_csv("./data_source/taxi_zone_lookup.csv")
-  top10_dropoff_districts_df = top10_dropoff_districts_df.merge(
-          taxi_zone_lookup_df,
-          left_on='do_location_id',
-          right_on='LocationID',
-          how='left'
-  )
+    query_speed = f'''
+        SELECT 
+          property_type,
+          SUM(total_customer)
+        FROM sale_speed.total_customer_by_property_type
+        WHERE 
+          created_at >= '{started_at}' 
+          AND created_at <= '{datetime.now()}'
+        GROUP BY property_type;
+    '''
+    df_2 = cursor.execute(query_speed).fetch_pandas_all()
 
-  return top10_dropoff_districts_df
+    merge_df = pd.concat([df_1.iloc[:, :2], df_2])
+    merge_df = merge_df.rename(columns={
+        merge_df.columns[0]: "property_type",
+        merge_df.columns[1]: "total_customer"
+    })
 
-def get_user_per_payment():
-  speed_query = f'''
-                  SELECT vendor_id, 
-                         payment_type 
-                  FROM user_per_payment_speed ;
-                '''
-  speed_data = session.execute(speed_query)
-  df = pd.DataFrame(speed_data)
+    total_customer_by_property_type_df = merge_df \
+                                              .groupby("property_type") \
+                                              .sum() \
+                                              .reset_index()
 
-  user_per_payment_df = df \
-                          .groupby('payment_type', as_index=False) \
-                          .sum() \
-                          .rename({'vendor_id' : 'count'}, axis=1) \
-                          .sort_values('count', ascending=False)
-  payment_type = ["Credit card", "Cash", "No charge", "Dispute", "Unknown", "Voided trip"]
-  user_per_payment_df['payment_name'] = payment_type[:user_per_payment_df.shape[0]]
+    return total_customer_by_property_type_df
+  except errors.Error as err:
+    if err.errno == errorcode.ER_FAILED_TO_CONNECT_TO_DB:
+      print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_NO_ACCOUNT_NAME:
+      print("Missing account name")
+    elif err.errno == errorcode.ER_NO_USER:
+      print("Missing username")
+    elif err.errno == errorcode.ER_NO_PASSWORD:
+      print("Missing password")
+    else:
+      print(err)
+  else:
+    conn.close()
 
-  return user_per_payment_df
+def query_total_customer_by_town(conn_config):
+  try:
+    conn = snowflake.connector.connect(**conn_config)
+    cursor = conn.cursor()
+
+    query_batch = '''
+        SELECT 
+          town,
+          SUM(total_customer),
+          started_at
+        FROM sale_batch.total_customer_by_town
+        GROUP BY 
+          town, 
+          started_at;
+    '''
+    df_1 = cursor.execute(query_batch).fetch_pandas_all()
+
+    started_at = df_1.iloc[0, 2]
+
+    query_speed = f'''
+        SELECT 
+          town,
+          SUM(total_customer)
+        FROM sale_speed.total_customer_by_town
+        WHERE 
+          created_at >= '{started_at}' 
+          AND created_at <= '{datetime.now()}'
+        GROUP BY town;
+    '''
+    df_2 = cursor.execute(query_speed).fetch_pandas_all()
+
+    merge_df = pd.concat([df_1.iloc[:, :2], df_2])
+    merge_df = merge_df.rename(columns={
+        merge_df.columns[0]: "town",
+        merge_df.columns[1]: "total_customer"
+    })
+
+    total_customer_by_town_df = merge_df \
+                                  .groupby("town") \
+                                  .sum() \
+                                  .reset_index()
+
+    return total_customer_by_town_df
+  except errors.Error as err:
+    if err.errno == errorcode.ER_FAILED_TO_CONNECT_TO_DB:
+      print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_NO_ACCOUNT_NAME:
+      print("Missing account name")
+    elif err.errno == errorcode.ER_NO_USER:
+      print("Missing username")
+    elif err.errno == errorcode.ER_NO_PASSWORD:
+      print("Missing password")
+    else:
+      print(err)
+  else:
+    conn.close()
 
 # dashboard title
-st.title("Real-Time NYC Taxi Dashboard")
+st.markdown("<h1 style='text-align: center;'>Real-Time Estate Sales Dashboard</h1>", unsafe_allow_html=True)
 
 # creating a single-element container
 placeholder = st.empty()
@@ -169,89 +275,57 @@ placeholder = st.empty()
 # near real-time / live feed simulation
 while True:
   with placeholder.container():
-    average_distance = get_average_trip_distance()
-    average_total_amount = get_average_total_amount()
-    total_passenger = get_total_passenger()
+    total_sale_amount = query_total_sale_amount(config)
+    total_customer = query_total_customer(config)
+    average_sale_ratio = query_average_sale_ratio(config)
 
     # create three columns
     kpi1, kpi2, kpi3 = st.columns(3)
 
     kpi1.metric(
-      label="Average Trip Distance",
-      value=round(average_distance, 3)
+      label="Total Sale Amount",
+      value=f"{round(total_sale_amount / 10**9, 2)} Billion $"
     )
         
     kpi2.metric(
-      label="Average Total Amount",
-      value=round(average_total_amount, 3)
+      label="Total Customer",
+      value=round(total_customer, 0)
     )
 
     kpi3.metric(
-      label="Total Passenger",
-      value=round(total_passenger, 2)
+      label="Avg Sale Ratio",
+      value=round(average_sale_ratio, 2)
     )
 
     # create two columns for charts
     fig_col1, fig_col2 = st.columns(2)
     with fig_col1:
-      st.markdown("Top 10 Pickup and Dropoff Places")
+      st.markdown("Customer per property type")
 
-      fig = plt.figure(figsize=(20, 15))
-      ax0 = fig.add_subplot(1, 2, 1)
-      ax1 = fig.add_subplot(1, 2, 2)
+      customer_per_property_type_df = query_total_customer_by_property_type(config)
 
-      sns.barplot(
-          data=get_top10_pickup_districts(),
-          x='count',
-          y='Zone',
-          color='cornflowerblue',
-          edgecolor='black',
-          ax=ax0
-      )
-
-      sns.barplot(
-          data=get_top10_dropoff_districts(),
-          x='count',
-          y='Zone',
-          color='cornflowerblue',
-          edgecolor='black',
-          ax=ax1
-      )
-      ax1.invert_xaxis()
-      ax1.yaxis.tick_right()
-      ax1.yaxis.set_label_position('right')
-
-      ax0.grid(color='black', linestyle=':', axis='x', dashes=(1,5), alpha=0.5)
-      ax1.grid(color='black', linestyle=':', axis='x', dashes=(1,5), alpha=0.5)
-
-      ax0.set_title('PICK UP PLACES')
-      ax1.set_title('DROP OFF PLACES')
-
-      ax0.set_ylabel('')
-      ax1.set_ylabel('')
-      ax0.set_xlabel('')
-      ax1.set_xlabel('')
-
-      for s in ['top', 'bottom', 'right']:
-          ax0.spines[s].set_visible(False)
-          
-      for s in ['top', 'bottom','left']:    
-          ax1.spines[s].set_visible(False)
-      
-      session.execute("TRUNCATE TABLE pickup_dropoff_speed;")
-      
+      fig = px.pie(
+                    customer_per_property_type_df, 
+                    values='total_customer', 
+                    names='property_type'
+                  )
+            
       st.write(fig)
             
     with fig_col2:
-      st.markdown("User per payment")
+      st.markdown("Customer per town")
       
-      fig = px.pie(get_user_per_payment(), 
-                   values='count', 
-                   names='payment_name', 
-                   title='User per payment')
+      customer_per_town_df = query_total_customer_by_town(config)
+      top_10_town_df = customer_per_town_df \
+                          .sort_values(by="total_customer", ascending=False) \
+                          .head(10)
 
-      session.execute("TRUNCATE TABLE user_per_payment_speed;")
+      fig = px.bar(
+                    top_10_town_df, 
+                    x='town', 
+                    y='total_customer'
+                  )
 
       st.write(fig)
 
-    time.sleep(45)
+    time.sleep(500)
