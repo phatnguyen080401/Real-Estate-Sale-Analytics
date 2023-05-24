684d6c80b3d6701c941e6cde010fa79bd267ff39
phatnguyen080401 <phat.nguyen@c2c-techhub.io>
2023-05-24 15:50:08 +0700

    Create gx dags

--- dags/batch_layer_dag.py
+++ dags/batch_layer_dag.py
@@ -4,6 +4,8 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
 
+from great_expectations_provider.operators.great_expectations import GreatExpectationsOperator
+
 default_args = {
   'owner': 'airflow',
   'depends_on_past': False,
@@ -11,15 +13,18 @@ default_args = {
   'retries': 0
 }
 
+GX_DIR = "/opt/airflow/great_expectations"
+
 with DAG('batch_layer_dag', default_args=default_args, catchup=False, max_active_runs=1, schedule="*/20 * * * *") as dag:
   total_customer_by_property_type_batch = BashOperator(
     task_id="total_customer_by_property_type_batch",
     bash_command="cd /opt/airflow/src/ && python /opt/airflow/src/batch_layer/total_customer_by_property_type_batch.py"
   )
 
-  total_customer_by_property_type_validation = BashOperator(
+  total_customer_by_property_type_validation = GreatExpectationsOperator(
     task_id="total_customer_by_property_type_validation",
-    bash_command="cd /opt/airflow/src/ && python /opt/airflow/src/test/batch_validations/total_customer_by_property_type_gx.py"
+    data_context_root_dir=GX_DIR,
+    checkpoint_name="total_customer_by_property_type_checkpoint",
   )
 
   total_customer_by_town_batch = BashOperator(
@@ -27,9 +32,10 @@ with DAG('batch_layer_dag', default_args=default_args, catchup=False, max_active
     bash_command="cd /opt/airflow/src/ && python /opt/airflow/src/batch_layer/total_customer_by_town_batch.py"
   )
 
-  total_customer_by_town_validation = BashOperator(
+  total_customer_by_town_validation = GreatExpectationsOperator(
     task_id="total_customer_by_town_validation",
-    bash_command="cd /opt/airflow/src/ && python /opt/airflow/src/test/batch_validations/total_customer_by_town_gx.py"
+    data_context_root_dir=GX_DIR,
+    checkpoint_name="total_customer_by_town_checkpoint",
   )
 
   total_sale_amount_ratio_batch = BashOperator(
@@ -37,9 +43,10 @@ with DAG('batch_layer_dag', default_args=default_args, catchup=False, max_active
     bash_command="cd /opt/airflow/src/ && python /opt/airflow/src/batch_layer/total_sale_amount_ratio_batch.py"
   )
 
-  total_sale_amount_ratio_validation = BashOperator(
+  total_sale_amount_ratio_validation = GreatExpectationsOperator(
     task_id="total_sale_amount_ratio_validation",
-    bash_command="cd /opt/airflow/src/ && python /opt/airflow/src/test/batch_validations/total_sale_amount_ratio_gx.py"
+    data_context_root_dir=GX_DIR,
+    checkpoint_name="total_sale_amount_ratio_checkpoint",
   )
 
   end = EmptyOperator(task_id="done", trigger_rule='all_success')
