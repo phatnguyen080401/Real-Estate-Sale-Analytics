1b3c058b90f9721c3edec2504663eae44a44483d
phatnguyen080401 <phat.nguyen@c2c-techhub.io>
2023-05-24 11:22:35 +0700

    Add validations to batch layer dag

--- dags/batch_layer_dag.py
+++ dags/batch_layer_dag.py
@@ -17,9 +17,19 @@ with DAG('batch_layer_dag', default_args=default_args, catchup=False, max_active
     bash_command="cd /opt/airflow/src/ && python /opt/airflow/src/batch_layer/total_customer_by_property_type_batch.py"
   )
 
+  total_customer_by_property_type_validation = BashOperator(
+    task_id="total_customer_by_property_type_validation",
+    bash_command="cd /opt/airflow/src/ && python /opt/airflow/src/test/batch_validations/total_customer_by_property_type_gx.py"
+  )
+
   total_customer_by_town_batch = BashOperator(
     task_id="total_customer_by_town_batch",
     bash_command="cd /opt/airflow/src/ && python /opt/airflow/src/batch_layer/total_customer_by_town_batch.py"
+  )
+
+  total_customer_by_town_validation = BashOperator(
+    task_id="total_customer_by_town_validation",
+    bash_command="cd /opt/airflow/src/ && python /opt/airflow/src/test/batch_validations/total_customer_by_town_gx.py"
   )
 
   total_sale_amount_ratio_batch = BashOperator(
@@ -27,10 +37,15 @@ with DAG('batch_layer_dag', default_args=default_args, catchup=False, max_active
     bash_command="cd /opt/airflow/src/ && python /opt/airflow/src/batch_layer/total_sale_amount_ratio_batch.py"
   )
 
+  total_sale_amount_ratio_validation = BashOperator(
+    task_id="total_sale_amount_ratio_validation",
+    bash_command="cd /opt/airflow/src/ && python /opt/airflow/src/test/batch_validations/total_sale_amount_ratio_gx.py"
+  )
+
   end = EmptyOperator(task_id="done", trigger_rule='all_success')
 
   (
-    total_customer_by_property_type_batch,
-    total_customer_by_town_batch,
-    total_sale_amount_ratio_batch
+    total_customer_by_property_type_batch >> total_customer_by_property_type_validation,
+    total_customer_by_town_batch >> total_customer_by_town_validation,
+    total_sale_amount_ratio_batch >> total_sale_amount_ratio_validation
   ) >> end
